eb52d52eb9b2652bae9d31fbebce0beffd376689
Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>
2023-07-11 23:28:55 +0200

    Support extensions using indexes reserved for internal organization (#17)
    Supplies a fallback mechanism for the deprecated extension index.
    Essentially we search for the index that we want, if its not found we
    look for the old one.
    
    depends on bufbuild/protovalidate#43

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bufbuild/protovalidate-go
 go 1.18
 
 require (
-	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.31.0-20230609233005-3757a25ff0b9.1
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.31.0-20230711201221-890d7e3584ce.1
 	github.com/envoyproxy/protoc-gen-validate v1.0.2
 	github.com/google/cel-go v0.16.0
 	github.com/stretchr/testify v1.8.4
@@ -13,11 +13,14 @@ require (
 require (
 	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/evaluator/resolver.go
+++ internal/evaluator/resolver.go
@@ -15,9 +15,18 @@
 package evaluator
 
 import (
+	"strings"
+
 	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/runtime/protoimpl"
+)
+
+const (
+	newExtensionIndex      = "1159"
+	previousExtensionIndex = "51071"
 )
 
 type StandardConstraintResolver interface {
@@ -29,15 +38,27 @@ type StandardConstraintResolver interface {
 type DefaultResolver struct{}
 
 func (r DefaultResolver) ResolveMessageConstraints(desc protoreflect.MessageDescriptor) *validate.MessageConstraints {
-	return resolveExt[protoreflect.MessageDescriptor, *validate.MessageConstraints](desc, validate.E_Message)
+	constraints := resolveExt[protoreflect.MessageDescriptor, *validate.MessageConstraints](desc, validate.E_Message)
+	if constraints == nil {
+		constraints = resolveDeprecatedIndex[protoreflect.MessageDescriptor, *validate.MessageConstraints](desc, validate.E_Message)
+	}
+	return constraints
 }
 
 func (r DefaultResolver) ResolveOneofConstraints(desc protoreflect.OneofDescriptor) *validate.OneofConstraints {
-	return resolveExt[protoreflect.OneofDescriptor, *validate.OneofConstraints](desc, validate.E_Oneof)
+	constraints := resolveExt[protoreflect.OneofDescriptor, *validate.OneofConstraints](desc, validate.E_Oneof)
+	if constraints == nil {
+		constraints = resolveDeprecatedIndex[protoreflect.OneofDescriptor, *validate.OneofConstraints](desc, validate.E_Oneof)
+	}
+	return constraints
 }
 
 func (r DefaultResolver) ResolveFieldConstraints(desc protoreflect.FieldDescriptor) *validate.FieldConstraints {
-	return resolveExt[protoreflect.FieldDescriptor, *validate.FieldConstraints](desc, validate.E_Field)
+	constraints := resolveExt[protoreflect.FieldDescriptor, *validate.FieldConstraints](desc, validate.E_Field)
+	if constraints == nil {
+		constraints = resolveDeprecatedIndex[protoreflect.FieldDescriptor, *validate.FieldConstraints](desc, validate.E_Field)
+	}
+	return constraints
 }
 
 // resolveExt does not use proto.GetExtension in the event the underlying type
@@ -45,30 +66,57 @@ func (r DefaultResolver) ResolveFieldConstraints(desc protoreflect.FieldDescript
 // circumstances, particularly in dynamic or runtime contexts, the underlying
 // extension value's type may be a dynamicpb.Message. In this case, we fall back
 // through a proto.[Un]Marshal cycle to get it into the concrete type we expect.
-func resolveExt[
-	D protoreflect.Descriptor,
-	C proto.Message,
-](
+func resolveExt[D protoreflect.Descriptor, C proto.Message](
 	desc D,
 	extType protoreflect.ExtensionType,
 ) (constraints C) {
 	opts := desc.Options().ProtoReflect()
 	fDesc := extType.TypeDescriptor()
 
-	if !opts.Has(fDesc) {
+	if opts.Has(fDesc) {
+		msg := opts.Get(fDesc).Message().Interface()
+		if m, ok := msg.(C); ok {
+			return m
+		}
+	}
+
+	unknown := opts.GetUnknown()
+	if len(unknown) == 0 {
 		return constraints
 	}
 
+	opts = opts.Type().New()
+	var resolver protoregistry.Types
+	if err := resolver.RegisterExtension(extType); err != nil {
+		return constraints
+	}
+	_ = proto.UnmarshalOptions{Resolver: &resolver}.Unmarshal(unknown, opts.Interface())
+
+	if opts.Has(fDesc) {
+		msg := opts.Get(fDesc).Message().Interface()
+		if m, ok := msg.(C); ok {
+			return m
+		}
+	}
 	msg := opts.Get(fDesc).Message().Interface()
 	if m, ok := msg.(C); ok {
 		return m
 	}
 
-	b, _ := proto.Marshal(msg)
-	constraints, ok := extType.New().Message().Interface().(C)
-	if !ok {
-		return constraints
-	}
-	_ = proto.Unmarshal(b, constraints)
 	return constraints
+}
+
+// resolveDeprecatedIndex is a fallback for the deprecated extension index.
+func resolveDeprecatedIndex[D protoreflect.Descriptor, C proto.Message](
+	desc D,
+	ext *protoimpl.ExtensionInfo,
+) C {
+	return resolveExt[D, C](desc, &protoimpl.ExtensionInfo{
+		ExtendedType:  ext.ExtendedType,
+		ExtensionType: ext.ExtensionType,
+		Field:         51071,
+		Name:          ext.Name,
+		Tag:           strings.Replace(ext.Tag, newExtensionIndex, previousExtensionIndex, 1),
+		Filename:      ext.Filename,
+	})
 }
