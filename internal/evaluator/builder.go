5a9fae13ee69efa176b7471a672e09dfb1624810
Oliver Sun <osun@buf.build>
2023-10-24 12:17:21 -0400

    add field's message descriptor itself as well

--- internal/evaluator/builder.go
+++ internal/evaluator/builder.go
@@ -275,6 +275,7 @@ func (bldr *Builder) processFieldExpressions(
 	if fieldDesc.Kind() == protoreflect.MessageKind {
 		opts = []cel.EnvOption{
 			cel.Types(dynamicpb.NewMessage(fieldDesc.ContainingMessage())),
+			cel.Types(dynamicpb.NewMessage(fieldDesc.Message())),
 			cel.Variable("this", cel.ObjectType(string(fieldDesc.Message().FullName()))),
 		}
 	} else {
