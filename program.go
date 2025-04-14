ebadf1c01f5a836e81d6b65f9c135f61d60aafe0
Steve Ayers <sayers@buf.build>
2025-04-14 16:14:55 -0400

    Map types

--- program.go
+++ program.go
@@ -75,7 +75,14 @@ func (s programSet) bindThis(val any) *variable {
 		//  traits.Mapper is not terribly feasible from this type.
 		m := make(map[any]any, value.Len())
 		value.Range(func(key protoreflect.MapKey, value protoreflect.Value) bool {
-			m[key.Interface()] = value.Interface()
+			switch key.Interface().(type) {
+			// case int32:
+			// 	m[key.Int()] = value.Interface()
+			// case uint32:
+			// 	m[key.Uint()] = value.Interface()
+			default:
+				m[key.Interface()] = value.Interface()
+			}
 			return true
 		})
 		binding.Val = m
