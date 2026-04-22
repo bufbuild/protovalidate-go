173027e1ea567eea4316ad874c0b4d555bc315aa
Jon Bodner <jbodner@buf.build>
2026-04-22 17:46:24 -0400

    fix issues in native_map.go

--- native_map.go
+++ native_map.go
@@ -16,6 +16,7 @@ package protovalidate
 
 import (
 	"fmt"
+	"math"
 
 	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"google.golang.org/protobuf/reflect/protoreflect"
@@ -26,43 +27,6 @@ var (
 	mapMinPairsDesc = (*validate.MapRules)(nil).ProtoReflect().Descriptor().Fields().ByName("min_pairs")
 	mapMaxPairsDesc = (*validate.MapRules)(nil).ProtoReflect().Descriptor().Fields().ByName("max_pairs")
 )
-
-// nativeMapEval is a native Go evaluator for map-level rules
-// (min_pairs, max_pairs). Key/value rules are handled separately
-// by the kvPairs evaluator in map.go.
-type nativeMapEval struct {
-	base
-	minPairs *uint64
-	maxPairs *uint64
-}
-
-func (n nativeMapEval) Evaluate(_ protoreflect.Message, val protoreflect.Value, _ *validationConfig) error {
-	size := uint64(val.Map().Len()) //nolint:gosec
-
-	// min_pairs
-	if n.minPairs != nil && size < *n.minPairs {
-		return n.newViolation(mapFieldRulesDesc, mapMinPairsDesc,
-			"map.min_pairs",
-			fmt.Sprintf("map must be at least %d entries", *n.minPairs),
-			val, protoreflect.ValueOfUint64(*n.minPairs))
-	}
-
-	// max_pairs
-	if n.maxPairs != nil && size > *n.maxPairs {
-		return n.newViolation(mapFieldRulesDesc, mapMaxPairsDesc,
-			"map.max_pairs",
-			fmt.Sprintf("map must be at most %d entries", *n.maxPairs),
-			val, protoreflect.ValueOfUint64(*n.maxPairs))
-	}
-
-	return nil
-}
-
-func (n nativeMapEval) Tautology() bool {
-	return false
-}
-
-var _ evaluator = nativeMapEval{}
 
 // tryNativeMapRules attempts to build a native Go evaluator for
 // map-level rules (min_pairs, max_pairs).
@@ -77,15 +41,15 @@ func tryNativeMapRules(base base, rules *validate.MapRules) evaluator {
 
 	hasRule := false
 
-	var minPairs *uint64
+	var minPairs uint64
 	if rules.HasMinPairs() {
-		minPairs = ptr(rules.GetMinPairs())
+		minPairs = rules.GetMinPairs()
 		hasRule = true
 	}
 
-	var maxPairs *uint64
+	var maxPairs uint64 = math.MaxUint64
 	if rules.HasMaxPairs() {
-		maxPairs = ptr(rules.GetMaxPairs())
+		maxPairs = rules.GetMaxPairs()
 		hasRule = true
 	}
 
@@ -98,4 +62,41 @@ func tryNativeMapRules(base base, rules *validate.MapRules) evaluator {
 		minPairs: minPairs,
 		maxPairs: maxPairs,
 	}
+}
+
+var _ evaluator = nativeMapEval{}
+
+// nativeMapEval is a native Go evaluator for map-level rules
+// (min_pairs, max_pairs). Key/value rules are handled separately
+// by the kvPairs evaluator in map.go.
+type nativeMapEval struct {
+	base
+	minPairs uint64
+	maxPairs uint64
+}
+
+func (n nativeMapEval) Evaluate(_ protoreflect.Message, val protoreflect.Value, _ *validationConfig) error {
+	size := uint64(val.Map().Len()) //nolint:gosec // int will never be negative or out of uint64 range
+
+	// min_pairs
+	if size < n.minPairs {
+		return n.newViolation(mapFieldRulesDesc, mapMinPairsDesc,
+			"map.min_pairs",
+			fmt.Sprintf("map must be at least %d entries", n.minPairs),
+			val, protoreflect.ValueOfUint64(n.minPairs))
+	}
+
+	// max_pairs
+	if size > n.maxPairs {
+		return n.newViolation(mapFieldRulesDesc, mapMaxPairsDesc,
+			"map.max_pairs",
+			fmt.Sprintf("map must be at most %d entries", n.maxPairs),
+			val, protoreflect.ValueOfUint64(n.maxPairs))
+	}
+
+	return nil
+}
+
+func (n nativeMapEval) Tautology() bool {
+	return n.minPairs == 0 && n.maxPairs == math.MaxUint64
 }
