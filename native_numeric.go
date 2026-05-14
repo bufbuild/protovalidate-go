800f2ccd3fbb4c5afd47f097f66647bace7eeaa7
Jon Bodner <jbodner@buf.build>
2026-05-14 11:52:04 -0400

    Improve formatting, NaN handling, and header validation (#320)
    While porting native rules to protovalidate-java, several issues were
    found in the protovalidate-go native rules implementation. This PR
    addresses those issues:
    
    - if a protovalidate rule specifies NaN for a gt/lt/gte/lte, fall
    through to the CEL rule implementation (these protovalidate rules are
    invalid, let CEL generate the error message)
    - in some cases, formatting for floating point numbers didn't match
    between CEL and native rules. Now they do.
    - the regular expression to validate a string field as a valid http
    header name was incorrect. It has been updated.

--- native_numeric.go
+++ native_numeric.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"math"
 	"slices"
+	"strconv"
 	"strings"
 
 	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
@@ -132,17 +133,25 @@ func tryBuildNativeNumericRules[T numericValue, R numericRules[T]](
 
 	hasRule := false
 
+	// bail out if there's a gt/lt and the value is NaN
+	// (it's an invalid protovalidate rule; let CEL return the error)
 	var lowerValue T
 	lower := lowerBoundNone
 	switch {
 	case rules.HasGt():
 		lower = lowerBoundGt
 		lowerValue = rules.GetGt()
+		if math.IsNaN(float64(lowerValue)) {
+			return nil
+		}
 		rules.ProtoReflect().Clear(config.descs.gtSite.desc)
 		hasRule = true
 	case rules.HasGte():
 		lower = lowerBoundGte
 		lowerValue = rules.GetGte()
+		if math.IsNaN(float64(lowerValue)) {
+			return nil
+		}
 		rules.ProtoReflect().Clear(config.descs.gteSite.desc)
 		hasRule = true
 	}
@@ -153,11 +162,17 @@ func tryBuildNativeNumericRules[T numericValue, R numericRules[T]](
 	case rules.HasLt():
 		upper = upperBoundLt
 		upperValue = rules.GetLt()
+		if math.IsNaN(float64(upperValue)) {
+			return nil
+		}
 		rules.ProtoReflect().Clear(config.descs.ltSite.desc)
 		hasRule = true
 	case rules.HasLte():
 		upper = upperBoundLte
 		upperValue = rules.GetLte()
+		if math.IsNaN(float64(upperValue)) {
+			return nil
+		}
 		rules.ProtoReflect().Clear(config.descs.lteSite.desc)
 		hasRule = true
 	}
@@ -470,16 +485,16 @@ func (n nativeNumericCompare[T]) gtltRule() string {
 
 func (n nativeNumericCompare[T]) loMessage() string {
 	if n.lower == lowerBoundGt {
-		return fmt.Sprintf("greater than %v", n.lo)
+		return "greater than " + format(n.lo)
 	}
-	return fmt.Sprintf("greater than or equal to %v", n.lo)
+	return "greater than or equal to " + format(n.lo)
 }
 
 func (n nativeNumericCompare[T]) hiMessage() string {
 	if n.upper == upperBoundLt {
-		return fmt.Sprintf("less than %v", n.hi)
+		return "less than " + format(n.hi)
 	}
-	return fmt.Sprintf("less than or equal to %v", n.hi)
+	return "less than or equal to " + format(n.hi)
 }
 
 func (n nativeNumericCompare[T]) conjunction() string {
@@ -496,7 +511,7 @@ func (n nativeNumericCompare[T]) Evaluate(_ protoreflect.Message, val protorefle
 	if n.constVal != nil && valT != *n.constVal {
 		violations = append(violations, n.newViolation(n.config.descs.constSite,
 			n.config.typeName+".const",
-			fmt.Sprintf("must equal %v", *n.constVal),
+			"must equal "+format(*n.constVal),
 			val, n.config.makeRuleVal(*n.constVal)))
 		if cfg.failFast {
 			return &ValidationError{Violations: violations}
@@ -601,7 +616,31 @@ func ptr[T any](v T) *T { return &v }
 func formatList[T any](vals []T) string {
 	parts := make([]string, len(vals))
 	for i, v := range vals {
-		parts[i] = fmt.Sprintf("%v", v)
+		parts[i] = format(v)
 	}
 	return "[" + strings.Join(parts, ", ") + "]"
+}
+
+func format(v any) string {
+	switch val := v.(type) {
+	case float32:
+		return printFloat(float64(val))
+	case float64:
+		return printFloat(val)
+	default:
+		return fmt.Sprintf("%v", val)
+	}
+}
+
+func printFloat(argDbl float64) string {
+	if math.IsNaN(argDbl) {
+		return "NaN"
+	}
+	if math.IsInf(argDbl, -1) {
+		return "-Infinity"
+	}
+	if math.IsInf(argDbl, 1) {
+		return "Infinity"
+	}
+	return strconv.FormatFloat(argDbl, 'f', -1, 64)
 }
